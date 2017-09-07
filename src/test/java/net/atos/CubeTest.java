f61fd4dbd1aa58a22826456ac2db2fd712532c39
gogsadmin <RedHat$1>
2017-09-07 18:36:02 +0100

    refactor and cleanup

--- src/test/java/net/atos/CubeTest.java
+++ src/test/java/net/atos/CubeTest.java
@@ -1,38 +1,20 @@
 package net.atos;
 
 import java.net.URL;
-import java.nio.file.Paths;
 
-import javax.ws.rs.core.Application;
-
-import org.arquillian.cube.CubeIp;
 import org.arquillian.cube.DockerUrl;
 import org.arquillian.cube.HostIp;
-import org.arquillian.cube.HostPort;
 
-import org.hamcrest.core.Is;
-
-//import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Assert;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import io.restassured.RestAssured;
-
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.StringContains.containsString;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 @RunWith(Arquillian.class)
